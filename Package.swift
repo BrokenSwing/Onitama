9791b64a1f0daab7bb0446c3398b10920ae15d66
unknown <allocompte@hotmail.fr>
2019-11-28 01:39:14 +0100

    Bump swift-tools version to 5.1

--- Package.swift
+++ Package.swift
@@ -1,9 +1,10 @@
+// swift-tools-version:5.1
 import PackageDescription
 
 let package = Package(
     name: "Onitama",
     platforms: [
-        .macOS(.v10_10)
+        .macOS(.v10_14)
     ],
     products: [
         .library(name: "Onitama", targets: ["Onitama"])
